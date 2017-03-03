1f2be5a5a450eefffde838935102c22df0234566
Trias Nur Rahman <contact@triasrahman.com>
2017-03-04 01:47:45 +0700

    Initial version

---
+++ Plugin.php
@@ -0,0 +1,40 @@
+<?php namespace Octobro\Midtrans;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Midtrans Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    public $require = ['Responsiv.Pay'];
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Midtrans',
+            'description' => 'Midtrans payment gateway for Responsiv.Pay plugin.',
+            'author'      => 'Octobro',
+            'icon'        => 'icon-credit-card'
+        ];
+    }
+
+    /**
+     * Registers any payment gateways implemented in this plugin.
+     * The gateways must be returned in the following format:
+     * ['className1' => 'alias'],
+     * ['className2' => 'anotherAlias']
+     */
+    public function registerPaymentGateways()
+    {
+        return [
+            'Octobro\Midtrans\PaymentTypes\Snap' => 'midtrans-snap',
+        ];
+    }
+}
---
+++ paymenttypes/Snap.php
@@ -0,0 +1,307 @@
+<?php namespace Octobro\Midtrans\PaymentTypes;
+
+use Input;
+use Flash;
+use Redirect;
+use Exception;
+use Veritrans_Snap;
+use Veritrans_Config;
+use ApplicationException;
+use Responsiv\Pay\Classes\GatewayBase;
+
+class Snap extends GatewayBase
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function gatewayDetails()
+    {
+        return [
+            'name'        => 'Snap',
+            'description' => 'Snap payment method from Midtrans.'
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function defineFormFields()
+    {
+        return 'fields.yaml';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function defineValidationRules()
+    {
+        return [
+            'server_key' => 'required',
+            'client_key' => 'required'
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function initConfigData($host)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function registerAccessPoints()
+    {
+        return array(
+            'snap_redirect' => 'processRedirect',
+            'snap_notify'   => 'processNotify',
+        );
+    }
+
+    /**
+     * Status field options.
+     */
+    public function getDropdownOptions()
+    {
+        return $this->createInvoiceStatusModel()->listStatuses();
+    }
+
+    /**
+     * Get the URL to Snap's servers
+     **/
+    public function getFormAction($host)
+    {
+        if ($host->test_mode) {
+            return "https://app.sandbox.veritrans.co.id/snap/snap.js";
+            return "https://app.sandbox.veritrans.co.id/snap/v1/pay";
+        }
+        else {
+            return "https://app.midtrans.com/snap/snap.js";
+        }
+    }
+
+    public function getClientKey($host)
+    {
+        return $host->client_key;
+    }
+
+    protected function getServerKey($host)
+    {
+        return $host->server_key;
+    }
+
+    public function getSnapToken($host, $invoice)
+    {
+		// Veritrans config
+		Veritrans_Config::$serverKey = $host->server_key;
+		Veritrans_Config::$isProduction = $host->test_mode ? false : true;
+		Veritrans_Config::$isSanitized = $host->is_sanitized ? true : false;
+		Veritrans_Config::$is3ds = $host->is_3ds ? true : false;
+
+		// Required
+        $totals = (object) $invoice->getTotalDetails();
+		$transactionDetails = array(
+			'order_id'     => $invoice->id,
+			'gross_amount' => (integer) $totals->total, // no decimal allowed for creditcard
+		);
+
+        $itemDetails = array_map(function($item) {
+            return array(
+                'id'       => rand(), // TODO: Get the item's ID
+                'price'    => (integer) $item['price'],
+                'quantity' => $item['quantity'],
+                'name'     => $item['description']
+            );
+        }, $invoice->getLineItemDetails());
+
+		// Optional
+        $customer = $invoice->getCustomerDetails();
+		$customerDetails = array(
+			'first_name'    => $customer['first_name'],
+			'last_name'     => $customer['last_name'],
+			'email'         => $customer['email'],
+			'phone'         => $customer['phone']
+		);
+
+		$enabledPayments = $this->getEnabledPayments($host->toArray());
+
+		// Fill transaction details
+		$transaction = array(
+			'enabled_payments'    => $enabledPayments,
+			'transaction_details' => $transactionDetails,
+			'customer_details'    => $customerDetails,
+			/* 'item_details'        => $itemDetails, */
+		);
+
+        try {
+            $snapToken = Veritrans_Snap::getSnapToken($transaction);
+        } catch (Exception $e) {
+            Flash::error($e->getMessage());
+            return;
+        }
+
+        return $snapToken;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function processPaymentForm($data, $host, $invoice)
+    {
+    }
+
+    public function processRedirect($params)
+    {
+        try {
+            $invoice = $this->createInvoiceModel()->find(get('order_id'));
+            if (!$invoice) {
+                throw new ApplicationException('Invoice not found');
+            }
+
+            return Redirect::to($invoice->getReceiptUrl());
+
+            //
+            // If wanna specialize the return type
+            //
+            // switch($params) {
+            //     case 'finish':
+            //     case 'unfinish':
+            //     case 'error':
+            // }
+        }
+        catch (Exception $ex)
+        {
+            if ($invoice) {
+                $invoice->logPaymentAttempt($ex->getMessage(), 0, [], $_GET, $response);
+            }
+
+            throw new ApplicationException($ex->getMessage());
+        }
+    }
+
+    public function processNotify($params)
+    {
+        try {
+			$response = Input::all();
+            $orderId = $response['order_id'];
+            $amount = $response['gross_amount'];
+
+            $invoice = $this->createInvoiceModel()
+                ->whereTotal($amount)
+                ->whereId($orderId)
+                ->first();
+
+            if (! $invoice) {
+                throw new ApplicationException('Invoice not found');
+            }
+
+            if (! $paymentMethod = $invoice->getPaymentMethod()) {
+                throw new ApplicationException('Payment method not found');
+            }
+
+            if ($paymentMethod->getGatewayClass() != 'Octobro\Midtrans\PaymentTypes\Snap') {
+                throw new ApplicationException('Invalid payment method');
+            }
+
+            if (! $this->isGenuineNotify($response, $invoice)) {
+                throw new ApplicationException('Hacker coming..');
+            }
+
+			$transactionStatus = $response['transaction_status'];
+			$paymentType = $response['payment_type'];
+            $statusMessage = $response['status_message'];
+
+            switch ($transactionStatus) {
+                case 'capture':
+                    if ($paymentType == 'credit_card') {
+
+                        $fraudStatus = $response['fraud_status'];
+
+                        if ($fraudStatus == 'challenge') {
+                            $invoice->updateInvoiceStatus($paymentMethod->invoice_challange_status);
+                        } else {
+                            if ($invoice->markAsPaymentProcessed()) {
+                                $invoice->logPaymentAttempt($statusMessage, 1, [], $response, null);
+                                $invoice->updateInvoiceStatus($paymentMethod->invoice_paid_status);
+                            }
+                        }
+                    }
+                    break;
+                case 'settlement':
+                    if ($invoice->markAsPaymentProcessed()) {
+                        $invoice->logPaymentAttempt($statusMessage, 1, [], $response, null);
+                        $invoice->updateInvoiceStatus($paymentMethod->invoice_settlement_status);
+                    }
+                    break;
+                case 'pending':
+                    $invoice->updateInvoiceStatus($paymentMethod->invoice_pending_status);
+                    break;
+                case 'deny':
+                case 'cancel':
+                case 'expire':
+                    $invoice->logPaymentAttempt($statusMessage, 0, [], $response, null);
+                    break;
+            }
+        } catch (Exception $ex) {
+            if ($invoice) {
+                $invoice->logPaymentAttempt($ex->getMessage(), 0, [], $_POST, null);
+            }
+
+            throw new ApplicationException($ex->getMessage());
+        }
+    }
+
+    public function isGenuineNotify($response, $invoice)
+    {
+       $generatedSignatureKey = $this->generateSignatureKey($response, $invoice);
+
+       if ($response['signature_key'] == $generatedSignatureKey) {
+           return true;
+       }
+
+       return false;
+    }
+
+    protected function generateSignatureKey($response, $invoice)
+    {
+        $orderId = $response['order_id'];
+        $statusCode = $response['status_code'];
+        $grossAmount = $response['gross_amount'];
+        $serverKey = $invoice->getPaymentMethod()->server_key;
+
+        $data = $orderId . $statusCode . $grossAmount . $serverKey;
+
+        $signature_key = openssl_digest($data, 'sha512');
+
+        return $signature_key;
+    }
+
+    protected function getEnabledPayments(array $host)
+    {
+        $channels = [
+            'is_credit_card'      => 'credit_card',
+            'is_mandiri_clickpay' => 'mandiri_clickpay',
+            'is_cimb_clicks'      => 'cimb_clicks',
+            'is_bri_epay'         => 'bri_epay',
+            'is_telkomsel_cash'   => 'telkomsel_cash',
+            'is_xl_tunai'         => 'xl_tunai',
+            'is_mandiri_ecash'    => 'mandiri_ecash',
+            'is_indosat_dompetku' => 'indosat_dompetku',
+            'is_bank_transfer'    => 'bank_transfer',
+            'is_echannel'         => 'echannel',
+            'is_cstore'           => 'cstore',
+        ];
+        $intersectChannels = array_intersect_key($host, $channels);
+
+        $enabledPayments = array_filter($intersectChannels, function($value) {
+            if ($value) return $value;
+        });
+
+        $enabledPayments = array_intersect_key($channels, $enabledPayments);
+
+        return array_keys(array_flip($enabledPayments));
+    }
+
+}
